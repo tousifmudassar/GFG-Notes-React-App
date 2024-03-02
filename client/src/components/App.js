e4771585f0960c257d3ff445b7e269d8c123a9c7
Mudassar Tousif <tousifmudassar@gmail.com>
2024-03-02 16:10:11 +0530

    Implemented a Header Component and Added Styles.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="App">
+        <Header className="Header" dark={true}>
+          GFG Notes App
+        </Header>
+      </div>
+    );
   }
 }
 
