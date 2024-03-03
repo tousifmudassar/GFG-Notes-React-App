aeb77605607897e4d49d6f841852e93bad424809
Mudassar Tousif <tousifmudassar@gmail.com>
2024-03-03 13:32:05 +0530

    Converted the Welcome into class component.

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import List from "./List";
-
-const Welcome = ({ User, handleLogOut }) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className="border rounded p-2">
-            <p>Welcome, {User.Name}.</p>
-            <button className="btn btn-danger btn-sm" onClick={handleLogOut}>
-              Logout
-            </button>
+class Welcome extends Component {
+  state = {
+    Notes: ["Note 1", "Note 2", "Note 3", "Note 4"],
+    CurrentNote: null
+  };
+  setCurrentNote = CurrentNote => {
+    this.setState({ CurrentNote });
+  };
+  render() {
+    const { User, handleLogOut } = this.props;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="border rounded p-2">
+              <p>Welcome, {User.Name}.</p>
+              <button className="btn btn-danger btn-sm" onClick={handleLogOut}>
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-3">
-          <List Notes={["Note 1", "Note 2", "Note 3", "Note 4"]} />
+        <div className="row mt-3">
+          <div className="col-3">
+            <List
+              Notes={this.state.Notes}
+              CurrentNote={this.CurrentNote}
+              setCurrentNote={this.setCurrentNote}
+            />
+          </div>
+          <div className="col-9">Right SideBar</div>
         </div>
-        <div className="col-9">Right SideBar</div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default Welcome;
