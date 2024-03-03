3bfee278d8b325ebb216dbf960a183c7629297ef
Mudassar Tousif <tousifmudassar@gmail.com>
2024-03-03 13:33:43 +0530

    Added actions to set and display the current Note.

--- client/src/components/Welcome/List.js
+++ client/src/components/Welcome/List.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-const List = ({ Notes }) => {
+const List = ({ Notes, setCurrentNote, CurrentNote }) => {
   return (
     <>
       <h3>Note Lists</h3>
       <ul className="list-group">
         {Notes.map((item, key) => (
-          <li className="list-group-item list-group-item-action" key={key}>
+          <li
+            className={
+              "list-group-item list-group-item-action" +
+              (CurrentNote === key ? "active" : "")
+            }
+            key={key}
+            onClick={() => setCurrentNote(key)}
+          >
             {item}
           </li>
         ))}
