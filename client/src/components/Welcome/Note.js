3c4f00d8411c3587d39904c6b4ed798ac01f7318
Mudassar Tousif <tousifmudassar@gmail.com>
2024-04-10 14:39:27 +0530

    Fixed Bug of Replace undefined and Removed currentNote.

--- client/src/components/Welcome/Note.js
+++ client/src/components/Welcome/Note.js
@@ -6,7 +6,7 @@ import {
   withRouter
 } from "react-router-dom";
 
-const Note = ({ CurrentNote, ...props }) => {
+const Note = ({ match }) => {
   return (
     <Switch>
       <Route path="/" exact={true}>
@@ -14,7 +14,7 @@ const Note = ({ CurrentNote, ...props }) => {
       </Route>
       <Route path="/:NoteID">
         <pre className="border rounded p-1 bg-light">
-          {JSON.stringify(props, null, 2)}
+          {JSON.stringify(match, null, 2)}
         </pre>
       </Route>
     </Switch>
