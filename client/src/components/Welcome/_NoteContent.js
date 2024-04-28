caf6efc4fd894d62151ac6e84ebcc1b44dc29de5
Mudassar Tousif <tousifmudassar@gmail.com>
2024-04-28 15:47:30 +0530

    Added Guard conditions for handling missing note.

--- client/src/components/Welcome/_NoteContent.js
+++ client/src/components/Welcome/_NoteContent.js
@@ -1,6 +1,27 @@
 import React from "react";
 
 const NoteContent = ({ match, Notes }) => {
+  if (Notes.length === 0) {
+    return (
+      <>
+        <button className="btn">
+          {/* <i className="fa fa-home"></i> */}
+          <i className="fa-solid fa-spinner"></i>
+        </button>
+        <h3 className="mb-3">Loading...</h3>
+      </>
+    );
+  }
+  const Note = Notes[+match.params.NoteID.replace("note-", "")];
+  if (!Note) {
+    return (
+      <>
+        <h3 className="mb-3">Note Not Found</h3>
+        <p>Oops! The Note you're looking for is not found!🙁</p>
+      </>
+    );
+  }
+
   const { Title, Desc } = Notes[+match.params.NoteID.replace("note-", "")];
   return (
     <>
